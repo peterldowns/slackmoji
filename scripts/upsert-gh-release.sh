029f639981dab09cc980512bad73e9e9ae7ad457
Peter Downs <peter@cloudx.io>
2026-08-20 15:52:13 -0400

    Use full commit SHA for release target

--- scripts/upsert-gh-release.sh
+++ scripts/upsert-gh-release.sh
@@ -6,13 +6,14 @@ set -euo pipefail
 
 version=$(cat ./VERSION)
 commit_sha=$(git rev-parse --short HEAD || echo 'unknown')
+target_commit=$(git rev-parse HEAD || echo 'main')
 # https://semver.org/#spec-item-10
 release_name="$version+commit.$commit_sha"
 if ! upload_url=$(
   gh api --method POST 'repos/{owner}/{repo}/releases' \
     -F "tag_name=$release_name" \
     -F "name=$release_name" \
-    -F "target_commitish=$commit_sha" \
+    -F "target_commitish=$target_commit" \
     --jq '.upload_url'
 ); then
   upload_url=$(gh api --method GET "repos/{owner}/{repo}/releases/tags/$release_name" --jq '.upload_url')
